a82ec860d3f25a510fd63c4319a6ed03e6738d07
Krishna Kumar <krishnak@thoughtworks.com>
2017-08-20 14:30:15 +0530

    [Krishna] Reading analog data from A0 and write it to serial port, works for MIC in

---
+++ src/Arduino/sample_analogRead.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  // print out the value you read:
+  Serial.println(sensorValue);
+//  Serial.write(sensorValue);
+}
